7c87290a706c19086e23fa882634cc34de26d14c
ionvop <ionvop@gmail.com>
2026-01-05 21:12:09 +0800

    Update index.php

--- recipes/index.php
+++ recipes/index.php
@@ -41,25 +41,46 @@ if (isset($_GET["category"])) {
                 </div>
                 <div style="
                     display: grid;
-                    grid-template-columns: repeat(2, 1fr);
+                    grid-template-columns: repeat(3, 1fr);
                     padding: 1rem;
                     padding-top: 2rem;">
                     <?php
                         if ($category == "all") {
-                            $query = <<<SQL
-                                SELECT * FROM `recipes`
-                            SQL;
+                            if (isset($_GET["q"])) {
+                                $query = <<<SQL
+                                    SELECT * FROM `recipes` WHERE `title` LIKE :q
+                                SQL;
 
-                            $stmt = $db->prepare($query);
-                            $recipes = $stmt->execute();
+                                $stmt = $db->prepare($query);
+                                $stmt->bindValue(":q", "%" . $_GET["q"] . "%");
+                                $recipes = $stmt->execute();
+                            } else {
+                                $query = <<<SQL
+                                    SELECT * FROM `recipes`
+                                SQL;
+
+                                $stmt = $db->prepare($query);
+                                $recipes = $stmt->execute();
+                            }
                         } else {
-                            $query = <<<SQL
-                                SELECT * FROM `recipes` WHERE `category` = :category
-                            SQL;
+                            if (isset($_GET["q"])) {
+                                $query = <<<SQL
+                                    SELECT * FROM `recipes` WHERE `title` LIKE :q AND `category` = :category
+                                SQL;
 
-                            $stmt = $db->prepare($query);
-                            $stmt->bindValue(":category", $category);
-                            $recipes = $stmt->execute();
+                                $stmt = $db->prepare($query);
+                                $stmt->bindValue(":q", "%" . $_GET["q"] . "%");
+                                $stmt->bindValue(":category", $category);
+                                $recipes = $stmt->execute();
+                            } else {
+                                $query = <<<SQL
+                                    SELECT * FROM `recipes` WHERE `category` = :category
+                                SQL;
+
+                                $stmt = $db->prepare($query);
+                                $stmt->bindValue(":category", $category);
+                                $recipes = $stmt->execute();
+                            }
                         }
 
                         while ($recipe = $recipes->fetchArray(SQLITE3_ASSOC)) {
@@ -88,6 +109,30 @@ if (isset($_GET["category"])) {
                         HTML;
                     }
                 ?>
+                <form style="
+                    padding: 1rem;">
+                    <div style="
+                        display: grid;
+                        grid-template-columns: max-content 1fr;
+                        border-bottom: 3px solid #a00;">
+                        <div style="
+                            display: flex;
+                            align-items: center;
+                            padding: 1rem;">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M380-320q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l224 224q11 11 11 28t-11 28q-11 11-28 11t-28-11L532-372q-30 24-69 38t-83 14Zm0-80q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
+                        </div>
+                        <div style="
+                            display: flex;
+                            align-items: center;">
+                            <input style="
+                                border: none;"
+                                type="text"
+                                name="q"
+                                placeholder="Search..."
+                                id="inputSearch">
+                        </div>
+                    </div>
+                </form>
                 <div style="
                     padding: 1rem;">
                     Categories:
@@ -125,9 +170,11 @@ if (isset($_GET["category"])) {
         <script src="script.js"></script>
         <script>
             const selectCategories = document.getElementById("selectCategories");
+            const inputSearch = document.getElementById("inputSearch");
             initialize();
 
             function initialize() {
+                inputSearch.value = <?= json_encode(isset($_GET["q"]) ? $_GET["q"] : "") ?>;
                 selectCategories.value = <?= json_encode($category) ?>;
             }
 
